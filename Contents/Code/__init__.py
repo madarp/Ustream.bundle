1f96426423580519929dbf6e5b282d1abc9deaff
sander1 <sander@plexapp.com>
2015-09-30 13:14:49 +0200

    Fix listing of categories and remove subcategory option.

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,6 +1,6 @@
-BASE_URL = 'http://www.ustream.tv'
-EXPLORE_URL = 'http://www.ustream.tv/new/explore/%s/all'
-AJAX_URL = 'http://www.ustream.tv/ajax-alwayscache/new/explore/%s/all.json?subCategory=%s&type=live&location=anywhere&page=%d'
+BASE_URL = 'https://www.ustream.tv'
+EXPLORE_URL = 'https://www.ustream.tv/explore/%s'
+AJAX_URL = 'https://www.ustream.tv/ajax-alwayscache/explore/%s/all.json?subCategory=&type=no-offline&location=anywhere&page=%d'
 RE_CATEGORY = Regex('explore/([^/]+)')
 
 ####################################################################################################
@@ -8,8 +8,7 @@ def Start():
 
 	ObjectContainer.title1 = 'Ustream'
 	HTTP.CacheTime = 60
-	HTTP.Headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/536.26.17 (KHTML, like Gecko) Version/6.0.2 Safari/536.26.17'
-	HTTP.Headers['Accept-Language'] = 'en-US,en;q=0.5'
+	HTTP.Headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36'
 
 ####################################################################################################
 @handler('/video/ustream', 'Ustream')
@@ -17,14 +16,16 @@ def MainMenu():
 
 	oc = ObjectContainer()
 
-	for category in HTML.ElementFromURL(BASE_URL, cacheTime=CACHE_1WEEK).xpath('//li[contains(@class, "categories")]/ul//a'):
-		title = category.text
+	for category in HTML.ElementFromURL(EXPLORE_URL % ('all'), cacheTime=CACHE_1WEEK).xpath('//div[contains(@class, "categories")]/ul//a'):
+
+		title = category.text.strip()
 		url = category.get('href')
 		category_id = RE_CATEGORY.search(url)
 
 		if category_id:
+
 			oc.add(DirectoryObject(
-				key = Callback(Subcategory, title=title, category_id=category_id.group(1)),
+				key = Callback(Streams, title=title, category_id=category_id.group(1)),
 				title = title
 			))
 
@@ -38,37 +39,17 @@ def MainMenu():
 	return oc
 
 ####################################################################################################
-@route('/video/ustream/{category_id}')
-def Subcategory(title, category_id):
-
-	oc = ObjectContainer(title2=title)
-	url = EXPLORE_URL % category_id
-
-	for subcategory in HTML.ElementFromURL(url, cacheTime=CACHE_1WEEK).xpath('//select[@id="FilterSubCategory"]/option'):
-		title = subcategory.text
-		subcategory_id = subcategory.get('value')
-
-		if subcategory_id == '':
-			continue
-
-		oc.add(DirectoryObject(
-			key = Callback(Streams, title=title, category_id=category_id, subcategory_id=subcategory_id, page=1),
-			title = title
-		))
-
-	return oc
-
-####################################################################################################
-@route('/video/ustream/{category_id}/{subcategory_id}/{page}', page=int)
-def Streams(title, category_id, subcategory_id, page=1):
+@route('/video/ustream/{category_id}/{page}', page=int)
+def Streams(title, category_id, page=1):
 
 	oc = ObjectContainer(title2=title, no_cache=True)
-	url = AJAX_URL % (category_id, subcategory_id, page)
+	url = AJAX_URL % (category_id, page)
 
 	json_obj = JSON.ObjectFromURL(url)
 	html = HTML.ElementFromString(json_obj['pageContent'])
 
 	for stream in html.xpath('//div[contains(@class, "media-item")]'):
+
 		url = stream.xpath('.//img/parent::a/@href')[0]
 		stream_title = stream.xpath('.//h4/a/text()')[0]
 		thumb = stream.xpath('.//img/@src')[0]
@@ -90,8 +71,9 @@ def Streams(title, category_id, subcategory_id, page=1):
 		return ObjectContainer(header="No content", message="This directory is empty")
 
 	if json_obj['pageMeta']['infinite'] == 1:
+
 		oc.add(NextPageObject(
-			key = Callback(Streams, title=title, category_id=category_id, subcategory_id=subcategory_id, page=page+1),
+			key = Callback(Streams, title=title, category_id=category_id, page=page+1),
 			title = 'More...'
 		))
 
